267237591295357eea30982ea82a760c3ae8ac1c
Bruna Santiago <brunasanthiago@outlook.com>
2023-09-20 17:08:32 -0300

    iniciando o card

--- src/components/CoffeList/index.tsx
+++ src/components/CoffeList/index.tsx
@@ -1,6 +1,6 @@
-import { ShoppingCart } from '@phosphor-icons/react'
+import { Minus, Plus, ShoppingCart } from '@phosphor-icons/react'
 import coffes from '../../data/coffes.json'
-import { Container, ListContainer } from './style'
+import { Container, ListContainer, Coffee, Description, Tag } from './style'
 
 export function CoffeeList() {
   return (
@@ -9,23 +9,33 @@ export function CoffeeList() {
       <ListContainer>
         {coffes.map((coffe) => {
           return (
-            <div key={coffe.nome}>
+            <Coffee key={coffe.nome}>
               <img src={coffe.imagem} alt={coffe.nome} />
-              <span>{coffe.tag}</span>
-              <h4>{coffe.nome}</h4>
-              <p>{coffe.descricao}</p>
-              <span>
-                {coffe.preco}
-                <div>
-                  <button>-</button>
-                  <p>0</p>
-                  <button>+</button>
-                  <button>
-                    <ShoppingCart />
-                  </button>
-                </div>
-              </span>
-            </div>
+              <Description>
+                <Tag>
+                  {coffe.tag.map((tags) => {
+                    return <h5 key={tags}>{tags}</h5>
+                  })}
+                </Tag>
+                <h4>{coffe.nome}</h4>
+                <p>{coffe.descricao}</p>
+                <span>
+                  {coffe.preco}
+                  <div>
+                    <button>
+                      <Minus />
+                    </button>
+                    <p>0</p>
+                    <button>
+                      <Plus />
+                    </button>
+                    <button>
+                      <ShoppingCart />
+                    </button>
+                  </div>
+                </span>
+              </Description>
+            </Coffee>
           )
         })}
       </ListContainer>
--- src/components/CoffeList/style.ts
+++ src/components/CoffeList/style.ts
@@ -3,11 +3,17 @@ import styled from 'styled-components'
 export const Container = styled.div`
   padding: 2rem 10rem;
   margin-left: 1.5rem;
+
   h1 {
     color: ${(props) => props.theme['base-subtitle']};
     font-family: 'Roboto', sans-serif;
     font-weight: bold;
     font-size: 1.5rem;
+  }
+
+  p {
+    display: flex;
+    align-items: center;
   }
 `
 
@@ -15,4 +21,51 @@ export const ListContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   margin-top: 54px;
+`
+
+export const Coffee = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${(props) => props.theme['base-card']};
+  width: 256px;
+  height: 310px;
+  margin: 1rem;
+  padding: 1rem;
+  border-radius: 6px 36px 6px 36px;
+
+  img {
+    margin-top: -2rem;
+  }
+`
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0.5rem;
+
+  h4 {
+    font-family: 'Baloo 2', cursive;
+    font-size: 1.2rem;
+    margin-top: 0.5rem;
+  }
+`
+
+export const Tag = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  margin-top: 0.4rem;
+  h5 {
+    color: ${(props) => props.theme['yellow-dark']};
+    background: ${(props) => props.theme['yellow-light']};
+    padding: 4px 8px;
+    border-radius: 100px;
+    font-size: 0.8rem;
+  }
+
+  p {
+    color: ${(props) => props.theme['base-label']};
+    font-family: 'Roboto', sans-serif;
+    align-self: center;
+  }
 `
